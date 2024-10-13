3cfb3e221f85a203f5eee16d9551966b4a65fb4d
andyppang <andyplpang@gmail.com>
2024-10-13 15:47:54 +0800

    update

---
+++ 20241013.py
@@ -0,0 +1,12 @@
+# 导入海龟库
+import turtle
+# 改变画笔颜色为蓝色
+turtle.color("blue")
+# 画一个半径为50的完整的圆
+turtle.circle(50)
+# 为半径增加一个负号，画一个反方向，半径为一百的圆
+turtle.circle(-100)
+# 增加一个参数，表示画圆时旋转的度数，此处度数90度，即绘制1/4个圆
+turtle.circle(150,90)
+turtle.circle(150,-90)
+turtle.circle(150,-90)
