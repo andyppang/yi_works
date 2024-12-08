5c92ddab8640f9ef593e4e8d440b737bd2695dd2
andyppang <andyplpang@gmail.com>
2024-12-08 15:54:16 +0800

    update

---
+++ 2024120803.py
@@ -0,0 +1,20 @@
+import turtle as t
+
+#t.fd(100)
+#t.rt(120)
+#t.fd(50)
+#t.rt(60)
+#t.fd(100)
+#t.rt(120)
+#t.fd(50)
+
+
+t.goto(100,0)
+t.goto(80,-20)
+t.goto(0,0)
+t.goto(-20,-20)
+t.goto(80,-20)
+
+
+
+
